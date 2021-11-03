f1709d401373d98eb542727fef1138de099effa3
Otavio Vittorassi <otavio@minhavisita.com.br>
2021-11-03 16:49:44 -0300

    Configurando canvas

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -11,10 +11,11 @@ export default {
     const script = p5 => {
       // These are your typical setup() and draw() methods
       p5.setup = () => {
-        p5.createCanvas(window.innerWidth, window.innerHeight);
+        p5.createCanvas(window.innerWidth, window.innerHeight, WEBGL);
       };
       p5.draw = () => {
         p5.clear();
+        p5.background(190);
         p5.ellipse(p5.mouseX, p5.mouseY, 40, 40);
       }; 
     }
